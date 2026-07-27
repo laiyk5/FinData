f794ce6e4c2be6f7a8d235f85cf78008338aeeab
Lai <laiyk5@163.com>
2026-07-27 14:05:26 +0800

    chore: remove scaffold test artifact

--- myco/provider/src/myco/plugins/providers/test/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Provider plugin for myco/test."""
-
-from myco.plugins.providers.test.provider import TestRuntime
-from myco.plugins.providers.test.provider import provider_plugin
-
-__all__ = ["TestRuntime", "provider_plugin"]
