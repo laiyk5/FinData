a87714028acaf8940284edf33c0040bf24783f4b
Lai <laiyk5@163.com>
2026-07-29 14:36:32 +0800

    chore: prepare v0.6.0 release

--- src/findata/__init__.py
+++ src/findata/__init__.py
@@ -4,4 +4,4 @@ from findata.sdk.contracts import DateRange, DatasetSpec, OperandError
 from findata.sdk.loader import DataLoader
 
 __all__ = ["DataLoader", "DateRange", "DatasetSpec", "OperandError"]
-__version__ = "0.4.0"
+__version__ = "0.6.0"
